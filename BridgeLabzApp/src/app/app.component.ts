6fddde0384d08037da7d6a5f1833ae9316b91e7f
Ankit Patel <yugap1806@gmail.com>
2025-03-07 05:27:32 +0530

    Ankit Patel Added UC2 - Display BridgeLabz Logo using Property Binding

--- BridgeLabzApp/src/app/app.component.ts
+++ BridgeLabzApp/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  message: string = "Hello from BridgeLabz"; 
+  message: string = "Hello from BridgeLabz";
+  logoUrl: string = "https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg";
 }
